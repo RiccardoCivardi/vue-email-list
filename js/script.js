19ff98149b384010d6e91d9c080d9caad516440b
RiccardoCivardi <riccardo.civardi@icloud.com>
2022-11-11 15:04:41 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+/*
+**Descrizione:**
+Attraverso l’apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
+**Bonus**
+Creare un loading e far comparire gli indirizzi email solamente quando sono stati TUTTI generati.
+*/
+
+const {createApp} = Vue;
+
+createApp({
+
+  data(){
+    return {
+
+
+
+
+
+    }
+  },
+
+  methods: {
+
+
+
+
+  },
+
+  mounted(){
+
+
+
+  }
+
+}).mount('#app');
